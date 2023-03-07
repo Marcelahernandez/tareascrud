23883df1b7e02b9894fc0d0a03de453566f22620
Marcelahernandez <yuliher11@hotmail.com>
2023-03-06 21:47:01 -0500

    Crud React

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CreateTodoButton.css';
+function CreateTodoButton(props){
+    const onClickButton = () =>{
+        props.setOpenModal(true)
+    }
+    return (
+        <button 
+        className="CreateTodoButton"
+        onClick={onClickButton}>
+            +
+        </button>
+    )
+}
+export { CreateTodoButton }
